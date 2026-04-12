092585e459709b69bed4f76d3672be116fa95e69
misticslippers <eternal.martyr13579@gmail.com>
2026-04-12 17:31:00 +0300

    update CreateEnrollmentRequest

--- src/main/java/me/ifmo/backend/DTO/enrollment/CreateEnrollmentRequest.java
+++ src/main/java/me/ifmo/backend/DTO/enrollment/CreateEnrollmentRequest.java
@@ -1,5 +1,6 @@
 package me.ifmo.backend.DTO.enrollment;
 
+import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -15,8 +16,10 @@ import lombok.Setter;
 public class CreateEnrollmentRequest {
 
     @NotNull(message = "User id must not be null")
+    @Min(value = 1, message = "User id must be greater than or equal to 1")
     private Long userId;
 
     @NotNull(message = "Course id must not be null")
+    @Min(value = 1, message = "Course id must be greater than or equal to 1")
     private Long courseId;
 }
