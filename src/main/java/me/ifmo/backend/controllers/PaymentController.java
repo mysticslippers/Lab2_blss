c680d50a60320682259dc2cb8366cf420eafcb36
misticslippers <eternal.martyr13579@gmail.com>
2026-04-12 17:35:45 +0300

    update PaymentController

--- src/main/java/me/ifmo/backend/controllers/PaymentController.java
+++ src/main/java/me/ifmo/backend/controllers/PaymentController.java
@@ -1,31 +1,34 @@
 package me.ifmo.backend.controllers;
 
+import jakarta.validation.constraints.Min;
 import lombok.RequiredArgsConstructor;
 import me.ifmo.backend.DTO.payment.PaymentDTO;
 import me.ifmo.backend.mappers.PaymentMapper;
 import me.ifmo.backend.services.PaymentService;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/payments")
+@Validated
 public class PaymentController {
 
     private final PaymentService paymentService;
     private final PaymentMapper paymentMapper;
 
     @PostMapping("/enrollment/{enrollmentId}")
-    public PaymentDTO createPayment(@PathVariable Long enrollmentId) {
+    public PaymentDTO createPayment(@PathVariable @Min(1) Long enrollmentId) {
         return paymentMapper.toPaymentDTO(paymentService.createPayment(enrollmentId));
     }
 
     @GetMapping("/{id}")
-    public PaymentDTO getPaymentById(@PathVariable Long id) {
+    public PaymentDTO getPaymentById(@PathVariable @Min(1) Long id) {
         return paymentMapper.toPaymentDTO(paymentService.getPaymentById(id));
     }
 
     @GetMapping("/enrollment/{enrollmentId}")
-    public PaymentDTO getPaymentByEnrollmentId(@PathVariable Long enrollmentId) {
+    public PaymentDTO getPaymentByEnrollmentId(@PathVariable @Min(1) Long enrollmentId) {
         return paymentMapper.toPaymentDTO(paymentService.getPaymentByEnrollmentId(enrollmentId));
     }
 }
