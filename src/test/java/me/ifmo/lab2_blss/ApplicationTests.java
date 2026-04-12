c9e0722ce8bfd63a090ca7f01a18fe3f029c3d10
misticslippers <eternal.martyr13579@gmail.com>
2026-04-12 16:45:08 +0300

    Initial commit

---
+++ src/test/java/me/ifmo/lab2_blss/ApplicationTests.java
@@ -0,0 +1,13 @@
+package me.ifmo.lab2_blss;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lab2BlssApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
