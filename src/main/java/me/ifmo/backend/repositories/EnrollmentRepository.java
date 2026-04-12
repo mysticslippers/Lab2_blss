62a454c1b5fbfefdf036076fcb6786bbffed48ea
misticslippers <eternal.martyr13579@gmail.com>
2026-04-12 17:31:59 +0300

    update EnrollmentRepository

--- src/main/java/me/ifmo/backend/repositories/EnrollmentRepository.java
+++ src/main/java/me/ifmo/backend/repositories/EnrollmentRepository.java
@@ -1,22 +1,13 @@
 package me.ifmo.backend.repositories;
 
 import me.ifmo.backend.entities.Enrollment;
-import me.ifmo.backend.entities.enums.EnrollmentStatus;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
-
-import java.time.LocalDateTime;
-import java.util.List;
 
 public interface EnrollmentRepository extends JpaRepository<Enrollment, Long> {
 
   Page<Enrollment> findAllByUserId(Long userId, Pageable pageable);
 
   Page<Enrollment> findAllByCourseId(Long courseId, Pageable pageable);
-
-  List<Enrollment> findAllByStatusAndPaymentExpiresAtBefore(
-          EnrollmentStatus status,
-          LocalDateTime time
-  );
 }
