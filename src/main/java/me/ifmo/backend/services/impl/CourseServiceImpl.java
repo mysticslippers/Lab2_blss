6121c5e859d4cb3188e6208fa171247c13321734
misticslippers <eternal.martyr13579@gmail.com>
2026-04-16 11:25:32 +0300

    update CourseServiceImpl

--- src/main/java/me/ifmo/backend/services/impl/CourseServiceImpl.java
+++ src/main/java/me/ifmo/backend/services/impl/CourseServiceImpl.java
@@ -2,13 +2,18 @@ package me.ifmo.backend.services.impl;
 
 import lombok.RequiredArgsConstructor;
 import me.ifmo.backend.entities.Course;
+import me.ifmo.backend.exceptions.BusinessException;
 import me.ifmo.backend.exceptions.NotFoundException;
 import me.ifmo.backend.repositories.CourseRepository;
+import me.ifmo.backend.repositories.specification.CourseSpecification;
 import me.ifmo.backend.services.CourseService;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.domain.Specification;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.math.BigDecimal;
 
 @Service
 @RequiredArgsConstructor
@@ -18,13 +23,27 @@ public class CourseServiceImpl implements CourseService {
     private final CourseRepository courseRepository;
 
     @Override
-    public Page<Course> getAllCourses(Pageable pageable) {
-        return courseRepository.findAllByIsActiveTrue(pageable);
-    }
+    public Page<Course> getCourses(
+            String title,
+            BigDecimal minPrice,
+            BigDecimal maxPrice,
+            Boolean isActive,
+            Boolean onlyAvailable,
+            Pageable pageable
+    ) {
+        if (minPrice != null && maxPrice != null && minPrice.compareTo(maxPrice) > 0) {
+            throw new BusinessException("minPrice must be less than or equal to maxPrice");
+        }
 
-    @Override
-    public Page<Course> getAllAvailableCourses(Pageable pageable) {
-        return courseRepository.findAllAvailableCourses(pageable);
+        Specification<Course> specification = Specification.allOf(
+                CourseSpecification.hasTitle(title),
+                CourseSpecification.hasMinPrice(minPrice),
+                CourseSpecification.hasMaxPrice(maxPrice),
+                CourseSpecification.hasActive(isActive),
+                CourseSpecification.onlyAvailable(onlyAvailable)
+        );
+
+        return courseRepository.findAll(specification, pageable);
     }
 
     @Override
