c8c320c5fc6cd3394352ec368a02b2f9b9a38e6a
misticslippers <eternal.martyr13579@gmail.com>
2026-04-16 11:24:59 +0300

    update CourseRepository

--- src/main/java/me/ifmo/backend/repositories/CourseRepository.java
+++ src/main/java/me/ifmo/backend/repositories/CourseRepository.java
@@ -1,20 +1,7 @@
 package me.ifmo.backend.repositories;
 
 import me.ifmo.backend.entities.Course;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
-public interface CourseRepository extends JpaRepository<Course, Long> {
-
-    Page<Course> findAllByIsActiveTrue(Pageable pageable);
-
-    @Query("""
-            SELECT course
-            FROM Course course
-            WHERE course.isActive = TRUE
-              AND course.availablePlaces > 0
-            """)
-    Page<Course> findAllAvailableCourses(Pageable pageable);
-}
+public interface CourseRepository extends JpaRepository<Course, Long>, JpaSpecificationExecutor<Course> {}
