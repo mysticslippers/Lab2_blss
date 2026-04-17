61bd13a4a0727f4094de14264a9a3ed65b6114cd
misticslippers <eternal.martyr13579@gmail.com>
2026-04-17 20:40:06 +0300

    update UserRepository

--- src/main/java/me/ifmo/backend/repositories/UserRepository.java
+++ src/main/java/me/ifmo/backend/repositories/UserRepository.java
@@ -3,4 +3,9 @@ package me.ifmo.backend.repositories;
 import me.ifmo.backend.entities.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface UserRepository extends JpaRepository<User, Long> {}
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    Optional<User> findByEmail(String email);
+}
