771e90df7aa5d2cefc7044e8db1e0cf9d9a4e95c
misticslippers <eternal.martyr13579@gmail.com>
2026-04-17 20:39:07 +0300

    update PaymentController

--- src/main/java/me/ifmo/backend/controllers/PaymentController.java
+++ src/main/java/me/ifmo/backend/controllers/PaymentController.java
@@ -5,6 +5,7 @@ import lombok.RequiredArgsConstructor;
 import me.ifmo.backend.DTO.payment.PaymentDTO;
 import me.ifmo.backend.mappers.PaymentMapper;
 import me.ifmo.backend.services.PaymentService;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
@@ -18,16 +19,19 @@ public class PaymentController {
     private final PaymentMapper paymentMapper;
 
     @PostMapping("/enrollment/{enrollmentId}")
+    @PreAuthorize("hasAuthority('PAYMENT_CREATE') and @accessService.canCreatePaymentForEnrollment(#enrollmentId, authentication)")
     public PaymentDTO createPayment(@PathVariable @Min(1) Long enrollmentId) {
         return paymentMapper.toPaymentDTO(paymentService.createPayment(enrollmentId));
     }
 
     @GetMapping("/{id}")
+    @PreAuthorize("hasAuthority('PAYMENT_READ_ALL') or @accessService.canAccessPayment(#id, authentication)")
     public PaymentDTO getPaymentById(@PathVariable @Min(1) Long id) {
         return paymentMapper.toPaymentDTO(paymentService.getPaymentById(id));
     }
 
     @GetMapping("/enrollment/{enrollmentId}")
+    @PreAuthorize("hasAuthority('PAYMENT_READ_ALL') or @accessService.canAccessPaymentByEnrollment(#enrollmentId, authentication)")
     public PaymentDTO getPaymentByEnrollmentId(@PathVariable @Min(1) Long enrollmentId) {
         return paymentMapper.toPaymentDTO(paymentService.getPaymentByEnrollmentId(enrollmentId));
     }
