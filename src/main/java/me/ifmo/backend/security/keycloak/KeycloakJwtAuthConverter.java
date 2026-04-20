9fddf00a619ad35f0c0272411dc43e38b329c44e
misticslippers <eternal.martyr13579@gmail.com>
2026-04-20 19:38:38 +0300

    update KeycloakJwtAuthConverter

--- src/main/java/me/ifmo/backend/security/keycloak/KeycloakJwtAuthConverter.java
+++ src/main/java/me/ifmo/backend/security/keycloak/KeycloakJwtAuthConverter.java
@@ -1,6 +1,7 @@
 package me.ifmo.backend.security.keycloak;
 
 import org.springframework.core.convert.converter.Converter;
+import org.springframework.lang.NonNull;
 import org.springframework.security.authentication.AbstractAuthenticationToken;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
@@ -8,13 +9,17 @@ import org.springframework.security.oauth2.jwt.Jwt;
 import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationToken;
 import org.springframework.stereotype.Component;
 
-import java.util.*;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
 
 @Component
 public class KeycloakJwtAuthConverter implements Converter<Jwt, AbstractAuthenticationToken> {
 
     @Override
-    public AbstractAuthenticationToken convert(Jwt jwt) {
+    public AbstractAuthenticationToken convert(@NonNull Jwt jwt) {
         Collection<GrantedAuthority> authorities = new HashSet<>();
 
         authorities.addAll(extractRealmRoles(jwt));
