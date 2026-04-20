282b99dcba1ecd08d72d5f78ae93db7eabbbca97
misticslippers <eternal.martyr13579@gmail.com>
2026-04-20 17:34:19 +0300

    update PaymentController

--- src/main/java/me/ifmo/backend/controllers/PaymentController.java
+++ src/main/java/me/ifmo/backend/controllers/PaymentController.java
@@ -5,6 +5,7 @@ import lombok.RequiredArgsConstructor;
 import me.ifmo.backend.DTO.payment.PaymentDTO;
 import me.ifmo.backend.mappers.PaymentMapper;
 import me.ifmo.backend.services.PaymentService;
+import me.ifmo.backend.services.impl.PaymentTransactionService;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
@@ -15,13 +16,14 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("/payments")
 public class PaymentController {
 
+    private final PaymentTransactionService paymentTransactionService;
     private final PaymentService paymentService;
     private final PaymentMapper paymentMapper;
 
     @PostMapping("/enrollment/{enrollmentId}")
     @PreAuthorize("hasAuthority('PAYMENT_CREATE') and @accessService.canCreatePaymentForEnrollment(#enrollmentId, authentication)")
     public PaymentDTO createPayment(@PathVariable @Min(1) Long enrollmentId) {
-        return paymentMapper.toPaymentDTO(paymentService.createPayment(enrollmentId));
+        return paymentMapper.toPaymentDTO(paymentTransactionService.createPayment(enrollmentId));
     }
 
     @GetMapping("/{id}")
